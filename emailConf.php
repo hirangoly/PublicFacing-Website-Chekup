a1597cc4e0d2fb4933b8cf3ef92d12c5aeced91c
Rangoly Jain <hirangoly@gmail.com>
2014-04-14 00:18:20 -0700

    Create emailConf.php

---
+++ emailConf.php
@@ -0,0 +1,18 @@
+[profile]
+PFcount = 100
+PFutm_source = katrina_PF
+
+
+[skill]
+SKcount = 2
+SKutm_source = katrina_SK
+
+
+[login]
+LGcount = 15
+LGutm_source = katrina_PV
+
+
+[Phone]
+PHcount = 8
+PHutm_source = katrina_PH
